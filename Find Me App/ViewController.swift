44b081d6447d3d4f4885fe14c641dca7cba67171
blaze <serj_rodriguez.93@icloud.com>
2016-03-10 12:33:55 -0600

    Initial Commit

---
+++ Find Me App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find Me App
+//
+//  Created by Sergio Rodriguez on 3/10/16.
+//  Copyright © 2016 Sergio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
